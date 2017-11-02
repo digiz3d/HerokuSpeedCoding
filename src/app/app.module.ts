fd1f59b4c24e71849dd2c1251021c2e23eff4d72
UndoRenegat <denisguilb@gmail.com>
2017-11-02 14:13:29 +0100

    Add formLogin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormLoginComponent } from './form-login/form-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormLoginComponent
   ],
   imports: [
     BrowserModule
